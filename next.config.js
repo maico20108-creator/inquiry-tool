96cd2098a5ad1f3bbb7994fa2e2d2d222f5800a8
maico20108-creator <mai.co.20108@gmail.com>
2026-05-06 18:39:03 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     serverComponentsExternalPackages: ['@prisma/client'],
   },
   env: {
-    DATABASE_URL: "postgresql://postgres.jnaqrsudfomugzogsanr:NH10DrYYMSR4qddc@aws-1-ap-south-1.pooler.supabase.com:6543/postgres?pgbouncer=true&connection_limit=1",
+    DATABASE_URL: "postgresql://postgres.jnaqrsudfomugzogsanr:NH10DrYYMSR4qddc@aws-1-ap-south-1.pooler.supabase.com:5432/postgres",
     DIRECT_URL: "postgresql://postgres.jnaqrsudfomugzogsanr:NH10DrYYMSR4qddc@aws-1-ap-south-1.pooler.supabase.com:5432/postgres",
   },
 }
