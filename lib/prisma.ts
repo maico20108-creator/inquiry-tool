2836a34495ffdbcdd0fc2322f54ef636bc336a67
maico20108-creator <mai.co.20108@gmail.com>
2026-05-07 17:55:04 +0900

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -9,7 +9,7 @@ export const prisma =
   new PrismaClient({
     datasources: {
       db: {
-        url: "postgresql://postgres.jnaqrsudfomugzogsanr:NH10DrYYMSR4qddc@aws-1-ap-south-1.pooler.supabase.com:5432/postgres",
+        url: "postgresql://postgres.jnaqrsudfomugzogsanr:76AMveeWRExdtjgS@aws-1-ap-south-1.pooler.supabase.com:5432/postgres",
       },
     },
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
